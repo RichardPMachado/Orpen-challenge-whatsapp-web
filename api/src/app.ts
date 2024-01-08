377e315c86d2aead7416622f1b12467fb58800be
RichardPMachado <richardmachado2211@gmail.com>
2024-01-08 16:29:02 -0300

    feat: app/ express config _RPM

---
+++ api/src/app.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from "express"
+import { userRouter } from "./Routes"
+// import ErrorHandler from ""
+
+const accessControl: express.RequestHandler = (
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,POST,DELETE,OPTIONS,PUT,PATCH'
+  )
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+}
+
+const app = express()
+
+// Configurando o middleware para analisar dados de formulário
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(accessControl)
+
+app.use('/user', userRouter)
+
+// app.use(ErrorHandler)
+
+export default app
