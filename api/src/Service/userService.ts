90694564a012d8f75eb6dce07b1b1d2f17a76965
RichardPMachado <richardmachado2211@gmail.com>
2024-01-09 03:04:18 -0300

    feat: login Service has been implemented _RPM

--- api/src/Service/userService.ts
+++ api/src/Service/userService.ts
@@ -1,4 +1,11 @@
-import { UserModel } from "../Model";
+import StatusCode from "status-code-enum"
+import validateField from '../Utils/serviceValidation'
+import ILoginResponse from "../Contracts/Interfaces/users/ILoginResponse"
+import IToken from "../Contracts/Interfaces/users/IToken"
+import { UserModel } from "../Model"
+import { compareHash } from "../Utils/User/hashPassword"
+import generateAccessInfo from "../Utils/User/generateAccessInfo"
+import CustomError from "../Errors/CustomError"
 
 export async function getUserById(id: number) {
   // _RPM
@@ -12,4 +19,35 @@ export async function getUserById(id: number) {
 export async function getAllUsers() {
   // _RPM
   return await UserModel.getAllUsers()
+}
+
+export async function login(loginUser: { nickName: string, password: string}): Promise<ILoginResponse & IToken> {
+  // Validação: Caso não tenha o formato correto, retorna erro 400.
+  // const validatedLogin = validateField<ILoginUser>(loginSchema, loginUser)
+
+  // Verifica se o nickName existe
+  const userFound = await UserModel.getUserByNickName(loginUser.nickName)
+
+  if (!userFound) {
+    throw new CustomError(
+      `${loginUser.nickName} ou senha incorretos`,
+      StatusCode.ClientErrorNotFound,
+    )
+  }
+
+  // Verifica se a senha coincide
+  const rightPassword = await compareHash(
+    loginUser.password,
+    userFound.password,
+  )
+
+  // Se a senha coincidir, gera token de acesso ao usuário; se não, retorna erro.
+  if (rightPassword) {
+    return generateAccessInfo(userFound)
+  } else {
+    throw new CustomError(
+      'Nome de usuário ou senha incorretos',
+      StatusCode.ClientErrorUnauthorized,
+    )
+  }
 }
