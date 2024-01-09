50b92d8c12e2e749f51958559cffb0aa2d2ae434
RichardPMachado <richardmachado2211@gmail.com>
2024-01-09 03:03:23 -0300

    feat: UserRouter login has been implemented _RPM

--- api/src/Routes/userRouter.ts
+++ api/src/Routes/userRouter.ts
@@ -1,10 +1,17 @@
 import { Router } from "express"
 import { UserController} from "../Controller"
+import nickNameRequired from "../Middlewares/nickNameRequired"
+import passwordRequired from "../Middlewares/passwordRequired"
 
 const userRouter = Router()
 
 userRouter.get('/', (req, res, next) => UserController.getAllUser(req, res, next))
 userRouter.get('/:id', (req, res, next) => UserController.getUserById(req, res, next))
 
-
+userRouter.post(
+  '/login',
+  nickNameRequired,
+  passwordRequired,
+  (req, res, next) => UserController.login(req, res, next),
+)
 export default userRouter
