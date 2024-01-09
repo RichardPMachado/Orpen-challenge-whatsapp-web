038ffecf515ac6157b473a8a0bdb8f348cc13554
RichardPMachado <richardmachado2211@gmail.com>
2024-01-09 01:46:08 -0300

    feat: Login Route web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,38 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-
+import { Routes, Route} from "react-router-dom"
+import Login from './Components/Login'
 function App() {
-  const [success, setSuccess] = useState('Acessar')
-
+// _RPM
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src="https://orpen.com.br/assets/img/logo-orpen.png" className="logo orpen" alt="Orpen logo" />
-        </a>
-      </div>
-      <h1>Challenge WhatsApp Web</h1>
-      <form action="">
-        <label htmlFor="login">Usuário</label>
-        <input id="login" type="text" />
-        <label htmlFor="password">senha</label>
-        <input id="password" type="password" />
-        <div className="card">
-          <button type="submit" onClick={(e) => {
-            e.preventDefault()
-            setSuccess('sucesso')
-          } }>
-            {success}
-          </button>
-          <p>
-            <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-      </form>
-      <p className="read-the-docs">
-        Click here if you want Richard to join the team!
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home />}/>
+    </Routes>
+
   )
 }
 
