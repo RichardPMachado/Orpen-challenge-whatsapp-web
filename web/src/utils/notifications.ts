b06354905b76a3c3c42ed175fc43378b0fc26c6a
RichardPMachado <richardmachado2211@gmail.com>
2024-01-12 15:29:35 -0300

    feat: mock notifications

---
+++ web/src/utils/notifications.ts
@@ -0,0 +1,27 @@
+export const notifications = [
+  {
+    id: 2,
+    recipient: 'Richard',
+    author: 'Rafael',
+    message: "Obrigado pela oportunidade ...",
+    createdAt: "2024-01-12T08:23:27.968Z",
+    newNotifications: 0,
+  },
+  {
+    id: 3,
+    recipient: 'Guilherme',
+    author: 'Rafael',
+    message: "Marcou a entrevista?",
+    createdAt: "2024-01-12T08:23:27.968Z",
+    newNotifications: 3,
+  },
+  {
+    id: 4,
+    recipient: 'Maicon',
+    author: 'Rafael',
+    message: "Amanhã as 13hs",
+    createdAt: "2024-01-12T08:23:27.968Z",
+    newNotifications: 1,
+
+  },
+]
