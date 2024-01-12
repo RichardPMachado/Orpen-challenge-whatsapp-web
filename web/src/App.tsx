507bce5b83a7de0f7578b83b36c8c546703a5b04
RichardPMachado <richardmachado2211@gmail.com>
2024-01-12 15:34:15 -0300

    feat:  react auth kit RequireAuth  config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,13 +2,18 @@
 import { Routes, Route} from "react-router-dom"
 import Login from './Components/Login'
 import Home from './Home'
+import { RequireAuth } from "react-auth-kit"
 function App() {
 // _RPM
   return (
     
     <Routes>
       <Route path="/" element={<Login/>}/>
-      <Route path="/home" element={<Home />}/>
+      <Route path="/home" element={
+        <RequireAuth loginPath={"/"}>
+          <Home />
+        </RequireAuth>
+      }  />
     </Routes>
 
   )
